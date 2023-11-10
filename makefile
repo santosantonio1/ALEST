9cf9ec7f1bc540612a32493a79431c882d9af367
Antônio <142094210+santosantonio1@users.noreply.github.com>
2023-11-09 21:19:01 -0300

    Add files via upload

--- makefile
+++ makefile
@@ -1,6 +1,8 @@
 CFLAGS=-c -std=c++11
 EXE1=t
-EXE2=bnt
+EXE2=ex5
 
 ${EXE1}:		
 				@g++ -o ${EXE1} rec_tree.cpp -D DEBUG
+${EXE2}:		
+				@g++ -o ${EXE2} ex5.cpp -D DEBUG
--- rec_tree.cpp
+++ rec_tree.cpp
@@ -18,55 +18,6 @@ struct node{
     }
 };
 
-template <typename T>
-struct b_node{
-    T info;
-    node *left,*right,*parent;
-    node(T i, node *l=nullptr, node *r=nullptr){
-        info = i; left = l; right = r;
-        if(left) left->parent = this;
-        if(right) right->parent = this;
-    }
-    ~node(){
-        // OBS: delete funciona recursivamente
-        if(left) delete left;
-        if(right) delete right;
-        #ifdef DEBUG
-        cout << "> Node " << info << " deleted" << endl;
-        #endif
-    }
-};
-
-template <typename T> int degree(node<T> *root){
-    int n=0;
-    if(root->left) n++;
-    if(root->right) n++;
-    return n;
-}
-
-template <typename T> int depth(node<T> *root){
-    int d=0;
-    node<T> *itr = root;
-    while(itr){
-        itr=itr->parent;
-        d++;
-    }
-    return d;
-}
-
-template <typename T> int height(node<T> * root){
-    if(!root) return -1;
-    int l = (root->left) ? (1 + height(root->left)) : 0;
-    int r = (root->right) ? (1+height(root->right)) : 0;
-    return (l>r) ? l : r;
-}
-
-template <typename T> int size(node<T> *root){
-    if(!root) return 0;
-    return 1 + size(root->left) + size(root->right);
-}
-
-
 /****** GENERATE GRAPHVIZ CODE FOR VISUALIZATION ******/
 template <typename T> string getNodes(node<T> *root){
     if(root==nullptr) return "";
@@ -130,29 +81,6 @@ template <typename T> void ex4(T type){
 
     cout << graphViz(root) << endl;
     clean(root);
-}
-
-template<typename> void ex5(T type){
-        b_node<T>*d = new b_node<T>('D');
-        b_node<T> *b = new b_node<T>('B', d, new b_node<T>('E'));
-        b_node<T> *f = new b_node<T>('F', new b_node<T>('H'), new node<T>('I'));
-        b_node<T> *c = new b_node<T>('C', f ,new b_node<T>('G'));
-        b_node<T> *root = new b_node<T>('A', b, c);
-
-        cout << graphViz(root);
-        cout << " degree(root): " << degree(root) << " [2] " << endl ;
-        cout << " degree(b): " << degree(b) << " [2] " << endl ;
-        cout << " degree(d): " << degree (d) << " [0] " << endl ;
-        cout << " depth(root): " << depth (root) << " [0] " << endl ;
-        cout << " depth(b): " << depth (b) << " [1] " << endl ;
-        cout << " depth(f): " << depth (f) << " [2] " << endl ;
-        cout << " size(root): " << size (root) << " [9]" << endl ;
-        cout << " size(b): " << size (b) << " [3] " << endl ;
-        cout << " size(c): " << size (c) << " [5] " << endl ;
-        cout << " height(root): " << height (root) << " [3] " << endl ;
-        cout << " height(b): " << height (b) << " [1] " << endl ;
-        cout << " height(c): " << height (c) << " [2] " << endl ;
-        delete root ;
 }
 
 int main(){
