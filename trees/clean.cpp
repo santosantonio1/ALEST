08cd7725402b37178e442878178455aa17441d1b
Antônio <142094210+santosantonio1@users.noreply.github.com>
2023-11-12 18:41:46 -0300

    Update clean.cpp

--- trees/clean.cpp
+++ trees/clean.cpp
@@ -1,3 +1,9 @@
+/*
+    NÃO DESCOBRI COMO IMPLEMENTAR UM make clean NO WINDOWS AINDA...
+    ENTÃO ESSE PROGRAMA LIMPA OS ARQUIVOS CRIADOS
+    É PRECISO ATUALIZAR A LISTA DE ARQUIVOS QUE DESEJA DELETAR
+*/
+
 #include <iostream>
 #include <fstream>
 #include <vector>
